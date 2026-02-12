ce6262b1bd1c3c2d8f995ae645266521cfd5ae26
m-schaar@gmx.net <m-schaar@gmx.net>
2026-02-12 19:12:32 +0100

    test: add login test basic

---
+++ app_auth/api/tests/test_login.py
@@ -0,0 +1,23 @@
+import pytest
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@pytest.mark.django_db
+def test_user_login_success(api_client):
+    url = reverse('login')
+    data = {
+        "email": "user@example.com",
+        "password": "securepassword!",
+    }
+
+    response = api_client.post(url, data, format='json')
+    response_data = response.json()
+
+    assert response.status_code == 200, f"Login fehlgeschlagen: {response.data}"
+
+    assert "user" in response_data
+    assert response_data["user"]["email"] == "user@example.com"
+    assert "id" in response_data["user"]
