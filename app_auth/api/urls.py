31b01cf913e419c4f7c8e9edc712214f23163d11
m-schaar@gmx.net <m-schaar@gmx.net>
2026-03-13 13:11:20 +0100

    .

--- app_auth/api/urls.py
+++ app_auth/api/urls.py
@@ -29,15 +29,3 @@ urlpatterns = [
         name="password_reset_confirm",
     ),
 ]
-    path("register/", RegistrationView.as_view(), name="register"),
-    path("activate/<str:uidb64>/<str:token>/", ActivateView.as_view(), name="activate"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogOutView.as_view(), name="logout"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
-    path("password_reset/", PasswordResetRequestView.as_view(), name="password_reset"),
-    path(
-        "password_confirm/<uidb64>/<token>/",
-        PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-]
