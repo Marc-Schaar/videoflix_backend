43979818fbbe2bd85e3c7b40259f1ad6ab7e4d67
m-schaar@gmx.net <m-schaar@gmx.net>
2026-03-13 08:36:27 +0100

    test: fix text with new file/folder structure

--- app_video/tests/test_video_flow.py
+++ app_video/tests/test_video_flow.py
@@ -1,6 +1,8 @@
 import os
 import pytest
+from django.conf import settings
 from app_video.models import VIDEO_RESOLUTIONS
+from app_video.tasks import delete_video_files_task
 
 
 @pytest.mark.django_db
@@ -31,7 +33,7 @@ def test_video_path_helpers(video_instance, fake_run_ffmpeg):
     assert os.path.exists(playlist_path)
     assert playlist_path.endswith(".m3u8")
 
-    video_dir = os.path.dirname(video.video_file.path)
+    video_dir = os.path.dirname(playlist_path)
     dummy_segment = "data01.ts"
     seg_full_path = os.path.join(video_dir, dummy_segment)
 
@@ -44,10 +46,9 @@ def test_video_path_helpers(video_instance, fake_run_ffmpeg):
 @pytest.mark.django_db
 def test_video_deletion_cleanup(video_instance, fake_run_ffmpeg):
     """Check that all associated files are removed from disk after deletion."""
-
     video = video_instance()
     video.refresh_from_db()
-
+    video_id = video.id
     paths_to_check = [video.video_file.path, video.thumbnail_url.path]
     for res in VIDEO_RESOLUTIONS.keys():
         paths_to_check.append(getattr(video, f"video_{res}").path)
@@ -56,6 +57,10 @@ def test_video_deletion_cleanup(video_instance, fake_run_ffmpeg):
         assert os.path.exists(p)
 
     video.delete()
+    delete_video_files_task(paths_to_check, video_id)
 
     for p in paths_to_check:
         assert not os.path.exists(p), f"Datei {p} wurde nicht gelöscht!"
+
+    video_dir = os.path.join(settings.MEDIA_ROOT, "videos", str(video_id))
+    assert not os.path.exists(video_dir), "ID-Ordner wurde nicht gelöscht!"
