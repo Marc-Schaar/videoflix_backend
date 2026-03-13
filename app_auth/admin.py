d424e16ce22a61e7049c362979188ec9ed24f1eb
m-schaar@gmx.net <m-schaar@gmx.net>
2026-03-13 17:48:02 +0100

    .

--- app_auth/admin.py
+++ app_auth/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'date_joined')
+
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+    search_fields = ('username', 'email')
+
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Persönliche Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Berechtigungen', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Wichtige Daten', {'fields': ('last_login', 'date_joined')}),
+    )
